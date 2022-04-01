12ac6514c15795e8fc35dfdf9b1cec0c49a6caf6
molniinosec@gmail.com <molniinosec@gmail.com>
2022-04-01 16:41:33 +0300

    4.1.1

--- RentOfEquipment/Windows/ListOfRent.xaml.cs
+++ RentOfEquipment/Windows/ListOfRent.xaml.cs
@@ -19,11 +19,18 @@ namespace RentOfEquipment.Windows
     /// </summary>
     public partial class ListOfRent : Window
     {
-        List<string> ListSort = new List<string> { "По умолчанию", "По Клиенту", "По Оборудованию", "По Сотруднику", "По Цене" };
+        List<string> ListSort = new List<string> { "По умолчанию", "По Клиенту", "По Сотруднику" };
         public ListOfRent()
         {
             InitializeComponent();
+            List<EF.ClientProduct> listRent = new List<EF.ClientProduct>();
+
+            listRent = ClassHelper.Appdata.Content.ClientProduct.ToList();
+            listRent = TotalCost(listRent);
             Filter();
+            cmbSor.ItemsSource = ListSort;
+            cmbSor.SelectedIndex = 0;
+
         }
 
         public List<EF.ClientProduct> TotalCost(List<EF.ClientProduct> list) 
@@ -42,11 +49,32 @@ namespace RentOfEquipment.Windows
 
             listRent = ClassHelper.Appdata.Content.ClientProduct.ToList();
 
-            listRent = TotalCost(listRent);
+
             listRent = listRent.Where(i => i.Client.LastName.ToLower().Contains(txtSearch.Text.ToLower()) 
             || i.Product.Name.ToLower().Contains(txtSearch.Text.ToLower()) 
             || i.Employee.SecondName.ToLower().Contains(txtSearch.Text.ToLower())).ToList();
 
+            switch (cmbSor.SelectedIndex)
+            {
+                case 0:
+                    {
+                        listRent = listRent.OrderBy(i => i.ID).ToList();
+                        break;
+                    }
+                case 1:
+                    {
+                        listRent = listRent.OrderBy(i => i.Client.LastName).ToList();
+                        break;
+                    }
+                case 2:
+                    {
+                        listRent = listRent.OrderBy(i => i.Employee.SecondName).ToList();
+                        break;
+                    }
+                default:
+                    listRent = listRent.OrderBy(i => i.ID).ToList();
+                    break;
+            }
             lvRent.ItemsSource = listRent;
         }
         private void cmbSor_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -57,16 +85,6 @@ namespace RentOfEquipment.Windows
         private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
             Filter();
-        }
-
-        private void lvClient_KeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void lvClient_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-
         }
     }
 }
